b15b8ddd16799d5cebdba18580e76b5c9ee05224
Amir Berenji <a.berenji62@gmail.com>
2024-07-16 13:59:24 -0700

    Make seed

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,8 @@
 
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -9,6 +13,17 @@ var app = builder.Build();
 app.UseAuthentication();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);    
+    throw;
+}
+
 
 app.Run();
 
